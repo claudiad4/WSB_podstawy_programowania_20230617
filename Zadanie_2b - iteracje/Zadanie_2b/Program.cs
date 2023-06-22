65bd5d6777186852924daef8d73fe0e67682ef1e
Claudia <135162896+claudiad4@users.noreply.github.com>
2023-06-22 21:59:14 +0200

    Add files via upload

---
+++ Zadanie_2b - iteracje/Zadanie_2b/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Wersja 2: Losowanie liczby parzystej od razu");
+        Console.Write("Podaj liczbę liczb parzystych do wyświetlenia: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Random random = new Random();
+        int count = 0;    // Licznik wylosowanych liczb parzystych
+        int number;
+
+        Console.Write("Wylosowane liczby parzyste: ");
+
+        while (count < n)
+        {
+            number = random.Next(-4, 5) * 2;  // Losowanie liczby parzystej od razu
+
+            Console.Write(number);
+
+            count++;
+
+            if (count < n)
+            {
+                Console.Write(", ");  // Dodanie przecinka i spacji poza ostatnią liczbą
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Liczba rzeczywistych losowań: " + count);
+
+        Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
+        Console.ReadKey();
+    }
+}
---
+++ Zadanie_3 - iteracje/Zadanie_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj wysokość trójkąta, którego chcesz zbudować: ");
+        int height = int.Parse(Console.ReadLine());
+
+        int maxWidth = height * 2 - 1; // Maksymalna szerokość trójkąta
+
+        for (int row = 1; row <= height; row++)
+        {
+            int numPlus = row * 2 - 1; // Liczba znaków '+' w danym wierszu
+            int numSpaces = (maxWidth - numPlus) / 2; // Liczba spacji przed znakami '+'
+
+            // Wypisanie spacji
+            for (int i = 0; i < numSpaces; i++)
+            {
+                Console.Write(" ");
+            }
+
+            // Wypisanie znaków '+'
+            for (int i = 0; i < numPlus; i++)
+            {
+                Console.Write("+");
+            }
+
+            Console.WriteLine(); // Przejście do nowego wiersza
+        }
+
+        Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
+        Console.ReadKey();
+    }
+}
