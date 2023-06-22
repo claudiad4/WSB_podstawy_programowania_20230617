b01d181f27a2f7c22b27e05f1efe98af1f6f43ee
Claudia <135162896+claudiad4@users.noreply.github.com>
2023-06-22 21:53:39 +0200

    Add files via upload

---
+++ Zadanie_4 - tablice/Zadanie_4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] tablica = { 14, 69, 74, 18, 41, 258, 781, 5, 3 };
+
+        Console.WriteLine("Tablica przed sortowaniem:");
+        WypiszTablice(tablica);
+
+        SortowaniePrzezWstawianie(tablica);
+
+        Console.WriteLine("Tablica po sortowaniu:");
+        WypiszTablice(tablica);
+    }
+
+    static void SortowaniePrzezWstawianie(int[] tablica)
+    {
+        int n = tablica.Length;
+
+        for (int i = 1; i < n; i++)
+        {
+            int klucz = tablica[i]; // Zapisanie wartości bieżącego elementu jako klucz
+            int j = i - 1;
+
+            // Przesuwanie większych elementów w prawo, aby zrobić miejsce dla klucza
+            while (j >= 0 && tablica[j] > klucz)
+            {
+                tablica[j + 1] = tablica[j];
+                j--;
+            }
+
+            tablica[j + 1] = klucz; // Wstawienie klucza na właściwą pozycję
+        }
+    }
+
+    static void WypiszTablice(int[] tablica)
+    {
+        for (int i = 0; i < tablica.Length; i++)
+        {
+            Console.Write(tablica[i]);
+
+            if (i < tablica.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Zadanie_5 - tablice/Zadanie_5/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] tablica = { 78, 15, 21, 98, 100, 215, 416, 49, 62 };
+
+        Console.WriteLine("Tablica przed sortowaniem:");
+        WypiszTablice(tablica);
+
+        SortowaniePrzezWybieranie(tablica);
+
+        Console.WriteLine("Tablica po sortowaniu:");
+        WypiszTablice(tablica);
+    }
+
+    static void SortowaniePrzezWybieranie(int[] tablica)
+    {
+        int n = tablica.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int indeksMinimum = i; // Przechowuje indeks najmniejszego elementu w nieposortowanej części tablicy
+
+            // Znajdowanie indeksu najmniejszego elementu w nieposortowanej części tablicy
+            for (int j = i + 1; j < n; j++)
+            {
+                if (tablica[j] < tablica[indeksMinimum])
+                {
+                    indeksMinimum = j;
+                }
+            }
+
+            // Zamiana miejscami najmniejszego elementu z pierwszym elementem nieposortowanej części
+            if (indeksMinimum != i)
+            {
+                ZamienElementy(tablica, i, indeksMinimum);
+            }
+        }
+    }
+
+    static void ZamienElementy(int[] tablica, int indeks1, int indeks2)
+    {
+        int temp = tablica[indeks1];
+        tablica[indeks1] = tablica[indeks2];
+        tablica[indeks2] = temp;
+    }
+
+    static void WypiszTablice(int[] tablica)
+    {
+        for (int i = 0; i < tablica.Length; i++)
+        {
+            Console.Write(tablica[i]);
+
+            if (i < tablica.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
