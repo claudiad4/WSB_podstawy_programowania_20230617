a05c36fa721dd89811a6049018f7cf4265d5fb77
Claudia <135162896+claudiad4@users.noreply.github.com>
2023-06-22 21:56:55 +0200

    Add files via upload

---
+++ Zadanie_1 - iteracje/Zadanie_1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    // Funkcja sprawdzająca, czy liczba jest liczbą pierwszą
+    static bool IsPrime(int num)
+    {
+        if (num < 2)
+            return false;
+
+        for (int i = 2; i < num; i++)
+        {
+            if (num % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    // Funkcja wypisująca n liczb pierwszych
+    static void PrintPrimeNumbers(int n)
+    {
+        int count = 0;  // Licznik znalezionych liczb pierwszych
+        int number = 2; // Aktualnie sprawdzana liczba
+
+        while (count < n)
+        {
+            if (IsPrime(number))
+            {
+                Console.WriteLine(number);
+                count++;
+            }
+
+            number++;
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Program wyświetla liczby pierwsze.");
+        Console.Write("Podaj liczbę liczb pierwszych do wyświetlenia: ");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintPrimeNumbers(n);
+
+        Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
+        Console.ReadKey();
+    }
+}
---
+++ Zadanie_2a - iteracje/Zadanie_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Wersja 1: Sprawdzanie poprawności losowania po losowaniu");
+        Console.Write("Podaj liczbę liczb parzystych do wyświetlenia: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Random random = new Random();
+        int count = 0;    // Licznik wylosowanych liczb parzystych
+        int number;
+
+        Console.Write("Wylosowane liczby parzyste: ");
+
+        while (count < n)
+        {
+            number = random.Next(-8, 9);  // Losowanie liczby z przedziału [-8, 8]
+
+            if (number % 2 == 0)  // Sprawdzenie, czy liczba jest parzysta
+            {
+                Console.Write(number);
+
+                count++;
+
+                if (count < n)
+                {
+                    Console.Write(", ");  // Dodanie przecinka i spacji poza ostatnią liczbą
+                }
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Liczba rzeczywistych losowań: " + count);
+
+        Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
+        Console.ReadKey();
+    }
+}
